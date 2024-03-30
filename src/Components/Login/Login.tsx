d50de35c4bd5682eaaec9988bc0ccfe3a5ab5139
saadk1580 <saadk1580@gmail.com>
2024-03-30 15:44:48 -0400

    add sign in button comp

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import "firebase/auth";
-import { GoogleAuthProvider, User, getRedirectResult, onAuthStateChanged, signInWithRedirect, signOut } from "firebase/auth";
-import { auth, db } from "../Auth/Auth";
-import { doc, setDoc } from "firebase/firestore";
-import { getUserDetails } from "../../utils/requests";
-import { Container, Heading, LoginBtn, ImgIcon } from "./Login.styles";
-import { useNavigate } from "react-router-dom";
-import styled from "@emotion/styled";
-import { Spinner } from "../Icons/Spinner";
+import { useEffect, useState } from 'react';
+import 'firebase/auth';
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '../Auth/Auth';
+import { doc, setDoc } from 'firebase/firestore';
+import { getUserDetails } from '../../utils/requests';
+import { Container, Heading } from './Login.styles';
+import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { Spinner } from '../Icons/Spinner';
+import { SignInWithGoogle } from '../SignInWithGoogle/SIgnInWithGoogle';
 
 const Main = styled.div({
-  width: "100vw",
-  height: "100vh",
+  width: '100vw',
+  height: '100vh',
 });
 
 export default function Login() {
-  const provider = new GoogleAuthProvider();
   const navigate = useNavigate();
 
   const [user, setUser] = useState<User | null>();
@@ -23,7 +23,7 @@ export default function Login() {
 
   onAuthStateChanged(auth, async (user) => {
     setUser(user);
-    user && setLoading(true)
+    user && setLoading(true);
   });
 
   const { uid, email, displayName, phoneNumber, photoURL, emailVerified } = user ?? {};
@@ -31,7 +31,7 @@ export default function Login() {
   useEffect(() => {
     const addUser = async () => {
       if (uid) {
-        const docRef = doc(db, "users", uid);
+        const docRef = doc(db, 'users', uid);
         const checkUser = await getUserDetails(uid);
 
         !checkUser &&
@@ -47,7 +47,7 @@ export default function Login() {
             chats: {},
           }));
       }
-      navigate("/chats");
+      navigate('/chats');
     };
 
     user && addUser();
@@ -59,10 +59,7 @@ export default function Login() {
         {!loading ? (
           <>
             <Heading>Welcome</Heading>
-            <LoginBtn onClick={() => signInWithRedirect(auth, provider)}>
-              <ImgIcon src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" width={25} />
-              Login with Google
-            </LoginBtn>
+            <SignInWithGoogle />
           </>
         ) : (
           <Spinner width={100} color="#ffffff" />
